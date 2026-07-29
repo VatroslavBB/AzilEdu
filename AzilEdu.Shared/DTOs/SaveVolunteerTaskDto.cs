e16f1902d51abcd09761158f461c6c2ccff5b910
VatroslavBB <vatrobb@gmail.com>
2026-07-30 00:21:57 +0200

    ui validation

--- AzilEdu.Shared/DTOs/SaveVolunteerTaskDto.cs
+++ AzilEdu.Shared/DTOs/SaveVolunteerTaskDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AzilEdu.Shared.DTOs;
 
-public class SaveVolunteerTaskDto
+public class SaveVolunteerTaskDto : IValidatableObject
 {
     [Required(ErrorMessage = "Naslov zadatka je obavezan.")]
     [StringLength(200, ErrorMessage = "Naslov može imati najviše 200 znakova.")]
@@ -25,4 +25,24 @@ public class SaveVolunteerTaskDto
 
     [Range(1, int.MaxValue, ErrorMessage = "Tip zadatka je obavezan.")]
     public int VolunteerTaskTypeId { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        // 4 = Završeno u tablici VolunteerTaskStatuses
+        var isCompleted = VolunteerTaskStatusId == 4;
+
+        if (isCompleted && CompletedAt is null)
+        {
+            yield return new ValidationResult(
+                "Zadatak sa statusom \"Završeno\" mora imati datum završetka.",
+                new[] { nameof(CompletedAt) });
+        }
+
+        if (!isCompleted && CompletedAt is not null)
+        {
+            yield return new ValidationResult(
+                "Datum završetka smije se unijeti samo kad je status \"Završeno\".",
+                new[] { nameof(CompletedAt) });
+        }
+    }
 }
